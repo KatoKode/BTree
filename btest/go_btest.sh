3a7678b162aaa43bb93de024768d2c29167a6967
Data Structures and Algorithms in C and Assembly Language <mcintosh1798@gmail.com>
2025-07-06 18:45:59 -0400

    Update go_btest.sh

--- btest/go_btest.sh
+++ btest/go_btest.sh
@@ -1,6 +1,6 @@
 # !/bin/sh
 #-------------------------------------------------------------------------------
-#   Assembly Language Implementation of a B-Tree
+#   B-Tree Implementation in x86_64 Assembly Language with C Interface
 #   Copyright (C) 2025  J. McIntosh
 #
 #   This program is free software; you can redistribute it and/or modify
